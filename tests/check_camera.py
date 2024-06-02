ad38f7d010358b31af24e7b8f1215dee7fa5a060
kazukingh01 <kazukingh01@gmail.com>
2024-06-02 16:39:44 +0900

    modify

--- tests/check_camera.py
+++ tests/check_camera.py
@@ -1,6 +1,10 @@
-import cv2
+import cv2, argparse
 
-cap = cv2.VideoCapture(0)
+parser = argparse.ArgumentParser()
+parser.add_argument("--camid", type=int, default=0)
+args   = parser.parse_args()
+
+cap = cv2.VideoCapture(args.camid)
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
